43ef8e44f962e4ea543dd2724a25b67124de18af
Jéssica <jehdg05@gmail.com>
2024-07-22 17:59:40 -0300

    Desenvolvimento e deploy do ChallengeAlura - Decodificador de texto

--- script.js
+++ script.js
@@ -1,34 +1,181 @@
-function decodeText() {
-    const inputText = document.getElementById('inputText').value;
-    const encodingType = document.getElementById('encodingType').value;
-    let decodedText = '';
+const html = document.querySelector("html");
+const textArea = document.querySelector(".js-textarea");
+const buttonToggle = document.querySelector(".js-btn-toggle");
+const buttonEncrypt = document.querySelector(".js-encrypt");
+const buttonDescrypt = document.querySelector(".js-descrypt");
+const display = document.querySelector(".js-display-text");
+const buttonCopy = document.querySelector(".js-btn-copy");
+const image = document.querySelector(".aside__image").cloneNode(true);
+const message = document.querySelector(".aside__message").cloneNode(true);
+const checkbox = document.querySelector(".js-checkbox");
+const buttonFullScreen = document.querySelector(".js-btn-fullscreen");
 
+textArea.focus();
+initTheme();
+
+function toggleTheme() {
+    html.classList.toggle("dark");
+
+    if(checkbox.checked) {
+        saveTheme("light");
+        console.log("light")
+
+    } else {
+        saveTheme("dark");
+        console.log("dark")
+
+    }
+}
+
+function saveTheme(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+function initTheme() {
+    const theme = localStorage.getItem("theme");
+
+    if(theme == "light") {
+        checkbox.checked = false;
+        html.classList.remove("dark");
+        
+    } else {
+        checkbox.checked = true;
+        html.classList.add("dark");
+    }
+}
+
+function pasteElement() {
+    display.textContent = "";
+    display.classList.remove("is-show-text");
+    display.appendChild(image);
+    display.appendChild(message);
+
+}
+
+function displayText(text) {
+    display.classList.add("is-show-text");
+    display.textContent = text;
+}
+
+function checkLowerCase() {
+
+    const regex = /^([a-z\s])+$/;
+    const lowerCase = regex.test(textArea.value);
+
+    if(lowerCase) {
+        encryptText();
+        
+    } else {
+        alert("Por favor, digite apenas letras minúsculas e sem acento.");
+    }
+}
+
+function encrypt(match) {
+    const keysOfEncrypt = {
+        "a": "ai",
+        "e": "enter",
+        "i": "imes",
+        "o": "ober",
+        "u": "ufat",
+    }
+
+    return keysOfEncrypt[match];
+}
+
+function scrollPage(ycoord) {
+    window.scroll({top: ycoord, behavior: "smooth"});
+}
+
+function encryptText() {
+    const text = textArea.value;
+
+    if(text != "") {
+
+        const encrypted = text.replace(/[aeiou]/g, encrypt);
+        displayText(encrypted);
+        scrollPage(html.scrollHeight);
+
+    }
+
+}
+
+function descrypt(match) {
+    const keysOfDescrypt = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u",
+    }
+    
+    return keysOfDescrypt[match];
+}
+
+function descryptText() {
+    const text = textArea.value;
+
+    if(text != "") {
+
+        const descrypted = text.replace(/ai|enter|imes|ober|ufat/g, descrypt); 
+        displayText(descrypted);
+        scrollPage(html.scrollHeight);
+    }
+}
+
+function copyText() {
+    const image = document.querySelector(".aside__image");
+
+    if(!display.contains(image)) {
+
+        navigator.clipboard.writeText(display.textContent).then(() => {
+            alert("Texto cópiado para a área de transferência");
+            pasteElement();
+            pasteText();
+            scrollPage(html.scrollHeight - html.scrollHeight);
+
+        })
+    
+    }
+
+}
+
+function pasteText() {
+    
     try {
-        switch (encodingType) {
-            case 'base64':
-                decodedText = atob(inputText);
-                break;
-            case 'url':
-                decodedText = decodeURIComponent(inputText);
-                break;
-            case 'hex':
-                decodedText = hexToString(inputText);
-                break;
-            default:
-                throw new Error('Tipo de codificação desconhecido');
-        }
-    } catch (error) {
-        decodedText = 'Erro na decodificação: ' + error.message;
-    }
+        textArea.focus();
+        textArea.value = "";
+        navigator.clipboard.readText().then((clipText) => {
+            textArea.value = clipText;
+        })
 
-    document.getElementById('outputText').value = decodedText;
+    } catch {
+        
+        alert("Erro: seu navegador não é compativel com a função de colar ou você não deu as permissões necessárias, use o atalho CRTL+V para colocar o texto cópiado");
+    }
+                  
+
 }
 
-function hexToString(hex) {
-    let str = '';
-    for (let i = 0; i < hex.length; i += 2) {
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+let fullScreen = false;
+
+function openAndCloseFullScreen(){
+    const html = document.querySelector("html");
+    const img = document.querySelector(".js-img-fullscreen");
+
+    if(fullScreen) {
+        document.exitFullscreen();
+        img.src = "./assets/icons-svg/full-screen.svg"
+        fullScreen = false;
+
+    } else {
+        html.requestFullscreen();
+        img.src = "./assets/icons-svg/full-screen-exit.svg"
+        fullScreen = true;
     }
-    return str;
 }
 
+buttonToggle.addEventListener("click", toggleTheme);
+buttonEncrypt.addEventListener("click", checkLowerCase);
+buttonDescrypt.addEventListener("click", descryptText);
+buttonCopy.addEventListener("click", copyText);
+buttonFullScreen.addEventListener("click", openAndCloseFullScreen);
